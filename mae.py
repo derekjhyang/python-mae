8d82ce9f8f68278ea0b396f8ea1c1e44e346d832
hswayne77 <hswayne77@gmail.com>
2013-07-10 05:26:34 +0800

    Create mae.py

---
+++ mae.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+def mae(pred_list, true_list):
+    if len(pred_list) != len(true_list):
+        raise Exception('Error: elements amount not match!')
+    return sum(map(lambda a,b: a-b,zip(pred_list, true_list))/len(true_list)
+    
+if __name__ == "__main__":
+   pass
