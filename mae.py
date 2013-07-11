10d801406fafe53a4df04801532fd176c0e96e95
SAMEVED <NCU.PDCLAB.SAMEVED@gmail.com>
2013-07-11 23:31:08 +0800

    fix mae function

--- mae.py
+++ mae.py
@@ -2,8 +2,10 @@
 
 def mae(pred_list, true_list):
     if len(pred_list) != len(true_list):
-        raise Exception('Error: elements amount not match!')
-    return sum(map(lambda a,b: a-b,zip(pred_list, true_list))/len(true_list)
+        raise Exception('Error: number of elements not match!')
+    return sum(map(lambda t:float(t[0]-t[1]),zip(pred_list, true_list)))/len(true_list)
     
 if __name__ == "__main__":
-   pass
+    a = [1,2,3]
+    b = [4,5,6]
+    print mae(a,b)
